6ccea6db97973d79e9c7e26988486c2f098e87c2
AMBER2136 <mikhail-grishkov@yandex.ru>
2022-08-31 17:46:21 +0300

    $ TEXT {X}

--- 30_08_2022/Task_2/Program.cs
+++ 30_08_2022/Task_2/Program.cs
@@ -4,14 +4,15 @@ Console.WriteLine("ВВЕДИТЕ ВТОРОЕ ЧИСЛО");
 double y = Convert.ToDouble(Console.ReadLine());
 if (x > y)
 {
-    Console.WriteLine("ВАШЕ ПЕРВОЕ ЧИСЛО БОЛЬШЕ ВТОРОГО");
+    Console.Write($"НАИБОЛЬШЕЕ ЧИСЛО РАВНО {x}");
 }
 else if (x == y)
 {
+
     Console.WriteLine("ВАШИ ЧИСЛА РАВНЫ");
 }
 else if (x < y)
 {
-    Console.WriteLine("ВАШЕ ВТОРОЕ ЧИСЛО БОЛЬШЕ ПЕРВОГО");
+    Console.Write($"НАИБОЛЬШЕЕ ЧИСЛО РАВНО {y}");
 }
 
