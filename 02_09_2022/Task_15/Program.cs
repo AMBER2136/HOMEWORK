f36a5ce06bb89a8a7cc41195ed2756c638b72b39
AMBER2136 <mikhail-grishkov@yandex.ru>
2022-09-03 22:09:37 +0300

    +Task_15

--- 02_09_2022/Task_15/Program.cs
+++ 02_09_2022/Task_15/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("ВВЕДИТЕ ЦИФРУ ОТ 1 ДО 7");
+int dig = Convert.ToInt32(Console.ReadLine());
+if (((dig > 7) && (dig<9))|| (dig == 0))
+{
+    Console.WriteLine("ВВЕДЕНА ЦИФРА ЗА ПРЕДЕЛАМИ ДИАПАЗОНА");
+    Console.WriteLine("ПОПРОБУЙТЕ ЕЩЕ РАЗ");
+}
+else
+if (dig > 9)
+{
+    Console.WriteLine("ВВЕДЕНО ЧИСЛО, А НЕ ЦИФРА");
+    Console.WriteLine("ПОПРОБУЙТЕ ЕЩЕ РАЗ");
+}
+else
+if (dig < 0)
+{
+    Console.WriteLine("ВВЕДЕНО ОТРИЦАТЕЛЬНОЕ ЧИСЛО");
+    Console.WriteLine("ПОПРОБУЙТЕ ЕЩЕ РАЗ");
+}
+else
+if (dig < 6)
+{
+    Console.WriteLine("НЕТ, ЭТО РАБОЧИЙ ДЕНЬ");
+}
+else
+if (dig > 5)
+{
+    Console.WriteLine("ДА, ЭТО ВЫХОДНОЙ ДЕНЬ");
+}
