c1f7669bd00e316668dae92c041aa5ef618adcee
AMBER2136 <mikhail-grishkov@yandex.ru>
2022-09-03 15:49:57 +0300

    03_09_2022_15:49

--- 02_09_2022/Task_10_1/Program.cs
+++ 02_09_2022/Task_10_1/Program.cs
@@ -1,1 +1,17 @@
 ﻿
+Console.WriteLine("ВВЕДИТЕ ТРЕХЗНАЧНОЕ ЧИСЛО");
+string a = Console.ReadLine();
+//Console.WriteLine(a);
+Second(a);
+void Second(string a)
+{
+    char[] num = a.ToCharArray();
+    int b = num.Length;
+    if ((b < 3) || (b > 3))
+    {
+        Console.WriteLine("ВВЕДЕННОЕ ЧИСЛО НЕ ТРЕХЗНАЧНОЕ");
+        Console.WriteLine("ПОПРОБУЙТЕ ЕЩЕ РАЗ");
+    }
+    else
+        Console.WriteLine($"ВТОРАЯ ЦИФРА ВВЕДЕННОГО ЧИСЛА - {num[1]}");
+}
--- 02_09_2022/Task_13_1/Program.cs
+++ 02_09_2022/Task_13_1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine("ВВЕДИТЕ ЦЕЛОЕ ЧИСЛО");
+string num = Console.ReadLine();
+char[] numm = num.ToCharArray();
+int size = numm.Length;
+
+if (size < 3)
+{
+    Console.WriteLine($"КОЛИЧЕСТВО ЦИФР ВО ВВЕДЕННОМ ЧИСЛЕ - {size}");
+    Console.WriteLine($"ПОРЯДОК ВВЕДЕННОГО ЧИСЛА - {size - 1}");
+    Console.WriteLine("ВО ВВЕДЕННОМ ЧИСЛЕ НЕТ ТРЕТЬЕЙ ЦИФРЫ");
+}
+else{
+Console.WriteLine($"КОЛИЧЕСТВО ЦИФР ВО ВВЕДЕННОМ ЧИСЛЕ - {size}");
+Console.WriteLine($"ПОРЯДОК ВВЕДЕННОГО ЧИСЛА - {size - 1}");
+Console.WriteLine($"ТРЕТЬЯ ЦИФРА ВВЕДЕННОГО ЧИСЛА - {numm[2]}");
+}
