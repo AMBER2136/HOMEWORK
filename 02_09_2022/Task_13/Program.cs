1523ffe78a55662e5e4dc8ccc90118cb2abbeff1
AMBER2136 <mikhail-grishkov@yandex.ru>
2022-09-03 15:05:45 +0300

    02_03_2022_15:05

--- 02_09_2022/Task_13/Program.cs
+++ 02_09_2022/Task_13/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("ВВЕДИТЕ ЧИСЛО");
+﻿Console.WriteLine("ВВЕДИТЕ ЦЕЛОЕ ЧИСЛО");
 int a = Convert.ToInt32(Console.ReadLine());
 int count = 0;
 int b = a;
@@ -10,25 +10,46 @@ while (b > 1)
 }
 if (count < 3)
 {
-    Console.WriteLine("У ЭТОГО ЧИСЛА НЕТ ТРЕТЬЕЙ ЦИФРЫ");
     int power = count - 1;
-    Console.WriteLine($"КОЛИЧЕСТВО ЦИФР В ЧИСЛЕ - {count}");
-    Console.WriteLine($"ПОРЯДОК ЧИСЛА - {power}");
+    Console.WriteLine($"КОЛИЧЕСТВО ЦИФР ВО ВВЕДЕННОМ ЧИСЛЕ - {count}");
+    Console.WriteLine($"ПОРЯДОК ВВЕДЕННОГО ЧИСЛА - {power}");
+    Console.WriteLine("ВО ВВЕДЕННОМ ЧИСЛЕ НЕТ ТРЕТЬЕЙ ЦИФРЫ");
 }
 else
 {
     int powe = count - 1;
-    Console.WriteLine($"КОЛИЧЕСТВО ЦИФР В ЧИСЛЕ - {count}");
-    Console.WriteLine($"ПОРЯДОК ЧИСЛА - {powe}");
+    Console.WriteLine($"КОЛИЧЕСТВО ЦИФР ВО ВВЕДЕННОМ ЧИСЛЕ ЧИСЛЕ - {count}");
+    Console.WriteLine($"ПОРЯДОК ВВЕДЕННОГО ЧИСЛА - {powe}");
 }
-if(count >2)
+if (count > 2)
 {
-while (count > 2)
-{
-    num = a % 10;
-    a = a / 10;
-    count--;
+    while (count > 2)
+    {
+        num = a % 10;
+        a = a / 10;
+        count--;
+    }
+    Console.WriteLine($"ТРЕТЬЯ ЦИФРА ВВЕДЕННОГО ЧИСЛА - {num}");
 }
-Console.WriteLine($"ТРЕТЬЯ ЦИФРА - {num}");
-}
+
+//ВАРИАНТ 2.
+
+// Console.WriteLine("ВВЕДИТЕ ЦЕЛОЕ ЧИСЛО");
+// string num = Console.ReadLine();
+// char[] numm = num.ToCharArray();
+// int size = numm.Length;
+
+// if (size < 3)
+// {
+//     Console.WriteLine($"КОЛИЧЕСТВО ЦИФР ВО ВВЕДЕННОМ ЧИСЛЕ - {size}");
+//     Console.WriteLine($"ПОРЯДОК ВВЕДЕННОГО ЧИСЛА - {size - 1}");
+//     Console.WriteLine("ВО ВВЕДЕННОМ ЧИСЛЕ НЕТ ТРЕТЬЕЙ ЦИФРЫ");
+// }
+// else{
+// Console.WriteLine($"КОЛИЧЕСТВО ЦИФР ВО ВВЕДЕННОМ ЧИСЛЕ - {size}");
+// Console.WriteLine($"ПОРЯДОК ВВЕДЕННОГО ЧИСЛА - {size - 1}");
+// Console.WriteLine($"ТРЕТЬЯ ЦИФРА ВВЕДЕННОГО ЧИСЛА - {numm[2]}");
+// }
+
+
 
