16850fb381f8320d17add88e59c037686c33d1fb
AMBER2136 <mikhail-grishkov@yandex.ru>
2022-09-03 14:02:05 +0300

    1,5_of_Task

---
+++ 02_09_2022/Task_10/Program.cs
@@ -0,0 +1,34 @@
+﻿// ВАРИАНТ 1.
+
+// Console.WriteLine("ВВЕДИТЕ ТРЕХЗНАЧНОЕ ЧИСЛО");
+// string a = Console.ReadLine();
+// //Console.WriteLine(a);
+// Second(a);
+// void Second(string a)
+// {
+//     char[] num = a.ToCharArray();
+//     int b = num.Length;
+//     if ((b < 3) || (b > 3))
+//     {
+//         Console.WriteLine("ВЫ ВВЕЛИ НЕ ТРЕХЗНАЧНОЕ ЧИСЛО");
+//     }
+//     else
+//         Console.WriteLine(num[1]);
+// }
+
+// ВАРИАНТ 2.
+Console.WriteLine("ВВЕДИТЕ ТРЕХЗНАЧНОЕ ЧИСЛО");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+int c = 0;
+if ((a < 100) || (a > 999))
+{
+    Console.WriteLine("ВЫ ВВЕЛИ НЕ ТРЕХЗНАЧНОЕ ЧИСЛО");
+    Console.WriteLine("ПОПРОБУЙТЕ ЕЩЕ РАЗ");
+}
+else
+{
+    b = a % 100;
+    c = b / 10;
+    Console.WriteLine(c);
+}
---
+++ 02_09_2022/Task_13/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("ВВЕДИТЕ ЧИСЛО");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int b = a;
+int num = 0;
+while (b > 1)
+{
+    b = b / 10;
+    count++;
+}
+if (count < 3)
+{
+    Console.WriteLine("У ЭТОГО ЧИСЛА НЕТ ТРЕТЬЕЙ ЦИФРЫ");
+    int power = count - 1;
+    Console.WriteLine($"КОЛИЧЕСТВО ЦИФР В ЧИСЛЕ - {count}");
+    Console.WriteLine($"ПОРЯДОК ЧИСЛА - {power}");
+}
+else
+{
+    int powe = count - 1;
+    Console.WriteLine($"КОЛИЧЕСТВО ЦИФР В ЧИСЛЕ - {count}");
+    Console.WriteLine($"ПОРЯДОК ЧИСЛА - {powe}");
+}
+if(count >2)
+{
+while (count > 2)
+{
+    num = a % 10;
+    a = a / 10;
+    count--;
+}
+Console.WriteLine($"ТРЕТЬЯ ЦИФРА - {num}");
+}
+
